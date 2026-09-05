d428d61561617fd7b7ad5487d0e95c976eda3338
Gurjot Singh <gsbsingh2410@gmail.com>
2026-09-05 10:08:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
 
-  base: '/Society-management-2/',
+  base: '/society-management-frontend-2/',
 
   server: {
     host: true,
